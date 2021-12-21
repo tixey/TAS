d8126ce390a8e09f1f2548edc8a39c7a7eef9c72
Nikita Tikhonov <tixey@mail.ru>
2021-12-21 09:09:51 +0200

    counterparties

--- counterparties.php
+++ counterparties.php
@@ -7,7 +7,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>TAON Company Limited - Accounting System</title>
+    <title>TAON Company Limited - Counterparties</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
